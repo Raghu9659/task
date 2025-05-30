9aafad9a6ff31d5f156a9a4f16f8d420f9aedfa7
ragu <raghupathiragu776@gmail.com>
2025-05-30 12:39:11 +0530

    python

---
+++ main.py
@@ -0,0 +1,170 @@
+#Name ="john"
+#name ="james"
+
+#age = "25"
+
+#name ='raghu' 
+#name ="""raghu"""
+
+"""
+this is a sample
+
+"""
+
+# x="alice"
+
+# name = "john"
+# age = 32
+
+#print("my name is " + name  + " and my age is ", age)
+
+#print(f"my name is {name} and my age is {age}")
+
+# print ("my name is {}, i am {} years old".format(name,age))
+
+# 'python'
+# 012345
+
+# str = "python"
+
+# print (str[3])
+
+# print (len(str))
+
+s = "Hello"
+#    12345678910
+# print (s[0:8:2])
+
+# s[0] = 'h'
+# s = "jello world"
+
+# s = 'j' + s[1:]
+
+# print(s)
+
+# print (s.upper())
+# print (s.lower())
+
+# print (s.strip())
+
+# print (s.replace("world", "python!"))
+# print (s.split(" "))
+
+# s_list = ['Hello', 'world', 'ragu']
+
+# print(" ". join(s_list))
+
+# print (s.count('l'))
+
+# print(s.isalpha())
+
+#a = -3j
+# print (type(a))
+
+#a = 10
+#b = 3
+
+# print (a + b)
+
+# print (a - b)
+
+# print (a / b)
+
+# print (a * b)
+
+# print (a //  b)
+
+# print (a % b)
+
+# print ( a ** b)
+
+# import math
+
+# print(math.sqrt(16))
+
+# print(math.pow(10,2))
+
+# print(math.floor(3.9999999))
+# print(math.ceil(3.01))
+
+# import random
+
+# print (random.random())
+
+# print (random.randint(1,10))
+
+# print (random.choice([2,5,7,9,3]))
+
+# DATA = 'value'
+
+# num = 10_00_000
+# print(type(num))
+
+# x = True
+# y = False
+
+# print(a and y)
+
+# print(x or y)
+
+# print (not x)
+
+# a = 10
+# b = 5
+# print (a==b)
+# print ( a<b)
+# print (a>b)
+# print (a!=b)
+# print (a <= b)
+# print (a >= b)
+
+# print (bool(None))
+
+# print (bool('raghu'))
+# print (bool(0))
+# print (bool(2))
+# print (bool(''))
+# print (bool ([1,2,3,4]))
+
+# a = 5
+# b = 1.2
+
+# print (a+b)
+
+# print(int(3.1256))
+
+# print(int(True))
+
+# print(float(10))
+
+# print (float("3.14"))
+
+# print(bool(" vgfdgh"))
+
+# price =int(input("Enter total amount: "))
+
+# total = price * 1.02
+
+# print(f"you have to pay {total}.")
+
+# age = int (input("Enter your age: "))
+
+# if age >=18:
+ #   print ("you are elgible vote.")
+
+# else:
+    # print ("you are not elgible to vote. ")
+
+# age = int (input("Enter your age: "))
+
+# if age < 5:
+    # print("you have to pay 50. ")
+
+# elif age<16:
+   # print("you have to pay 100. ")
+
+# else:
+  #   print("you have to pay 200. ")
+    
+for i in range(10):
+    print(i)
